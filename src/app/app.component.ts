ba33d9e14fc735a78ac5c7b2b090291b738c1c33
Jair Araujo <jaraujod1@upao.edu.pe>
2024-07-12 17:01:56 -0500

    fix: Se cambió la palabra mal escrita

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { EmployeeFormComponent } from './components/input-employee-form/input-employee-form.component';
+import { InputEmployeeFormComponent } from './components/input-employee-form/input-employee-form.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  imports: [EmployeeFormComponent]
+  imports: [InputEmployeeFormComponent]
 })
 export class AppComponent {
   title = 'Calculadora de Salarios';
